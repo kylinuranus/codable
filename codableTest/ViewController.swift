5edbd9fc80e60fea7b71030236ebefd9f3f89c0a
张起灵 <kylinuranus@gmail.com>
2019-07-16 23:34:25 +0800

    Initial Commit

---
+++ codableTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  codableTest
+//
+//  Created by 赵申侃 on 2019/7/16.
+//  Copyright © 2019 赵申侃. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
