a574591464671d83cfc751fd6c88e1a38f202002
张起灵 <kylinuranus@gmail.com>
2019-07-18 00:13:24 +0800

    codable

--- codableTest/ViewController.swift
+++ codableTest/ViewController.swift
@@ -12,9 +12,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let json = #"""
+             {
+                 "boolean": true,
+                 "integer": 1,
+                 "double": -3.14159265358979323846,
+                 "string": "string",
+                 "array": ["1","2","3"]
+             }
+        """#.data(using: .utf8)!
+        
+        let p:PStruct = try! JSONDecoder().decode(PStruct.self, from: json)
+        
+        
+        print(p)
+
     }
 
+}
 
+struct PStruct:Decodable {
+    
+    var boolean:Bool
+    var integer:Int
+    var double:Double
+    var string:String
+    var array:[String]
+    
+    
 }
 
